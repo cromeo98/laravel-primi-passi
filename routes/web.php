a3f959179c9c9281c6c9556bdadbfe7cac2e38ff
cromeo98 <romeo.christian98@gmail.com>
2021-09-03 15:57:46 +0200

    hello world init

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('home');
+})->name('homepage');
+
+Route::get('/about', function () {
+    return view('about');
+})->name('chi-siamo');
